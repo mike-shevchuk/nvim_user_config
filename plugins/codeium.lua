7e4cb3a79f01ccb64a6c0598456af6004fdd91b5
Mike Shevchuk <tetraedrgo@gmail.com>
2024-02-21 15:10:26 +0200

    Enable plugin codeium

--- plugins/codeium.lua
+++ plugins/codeium.lua
@@ -1,13 +1,15 @@
 return {
-    -- "Exafunction/codeium.nvim",
-    -- event = 'BufEnter',
+    "Exafunction/codeium.nvim",
+    -- event = "BufEnter",
     -- enable = true,
     -- lazzy = false,
-    -- dependencies = {
-    --     "nvim-lua/plenary.nvim",
-    --     "hrsh7th/nvim-cmp",
-    -- },
-    -- config = function()
-    --     require("codeium").setup({})
-    -- end,
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "hrsh7th/nvim-cmp",
+    },
+    config = function()
+        require("codeium").setup({})
+    end,
+    
+    
 }

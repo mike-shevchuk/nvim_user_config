f240f3a3d877482bb119bb635987b01f79ea729c
Mike Shevchuk <tetraedrgo@gmail.com>
2024-02-21 15:34:26 +0200

    update telekasten

--- plugins/telekasten.lua
+++ plugins/telekasten.lua
@@ -4,12 +4,23 @@ return {
   config = function()
     require('telekasten').setup({
       home = vim.fn.expand("~/zettelkasten"), -- Put the name of your notes directory here
+      dailies = vim.fn.expand("~/zettelkasten/comb-notes/daily_staff"), -- where our data files will be stored
+      weeklies = vim.fn.expand("~/zettelkasten/comb-notes/weekly_staff"), -- where our data files will be stored 
+      templates = vim.fn.expand("~/zettelkasten/comb-notes/trash"), -- Dir for our template files
+      media_previewer = "viu-previewer",
+      image_link_style = "markdown",
+      -- media_previewer = "catimg-previewer",
+      --
+      -- 
+
+
     })
     -- Launch panel if nothing is typed after <leader>z
     vim.keymap.set("n", "<leader>z", "<cmd>Telekasten panel<CR>")
 
     -- Most used functions
     vim.keymap.set("n", "<leader>zf", "<cmd>Telekasten find_notes<CR>")
+    vim.keymap.set("n", "<leader>zt", "<cmd>Telekasten show_tags<CR>")
     vim.keymap.set("n", "<leader>zg", "<cmd>Telekasten search_notes<CR>")
     vim.keymap.set("n", "<leader>zd", "<cmd>Telekasten goto_today<CR>")
     vim.keymap.set("n", "<leader>zz", "<cmd>Telekasten follow_link<CR>")
@@ -20,5 +31,6 @@ return {
 
     -- Call insert link automatically when we start typing a link
     vim.keymap.set("i", "[[", "<cmd>Telekasten insert_link<CR>")
+    
   end,
 }
